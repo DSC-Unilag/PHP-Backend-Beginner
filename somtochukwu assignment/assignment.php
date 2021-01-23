d6b99062ed00d1eaab9e63224c053925f29d2e99
somT-oss <somtochukwuuchegbu@gmail.com>
2021-01-23 16:44:13 +0100

    Just commited my week one assignment

---
+++ somtochukwu assignment/assignment.php
@@ -0,0 +1,35 @@
+<?php
+
+    // Question 1 
+    $name = "Somtochukwu";
+    $age = 17;
+    $country = "Nigeria";
+
+    echo $name, $age, $country;
+
+    // Question 2
+    echo "My name is " . $name ." I am "
+    . $age . "years old. I'm from ". $country;
+
+    // Question 3
+    define("population", "18,000,000");
+    echo population;
+
+
+    // Question 4
+    echo gettype("bolanle");
+    echo gettype(287);
+    echo gettype(28.7);
+    echo gettype(false);
+
+
+    // Question 2
+    $x = 108;
+    $y = 54;
+    $squareX = $x ** 2;
+    $halfY = $y / 2;
+    echo $x + $y;
+    echo $squareX * $halfY;
+    echo $x % $y;
+
+?>
