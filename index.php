fc10f86f4b27935733a9744e69f59271adfe2d4c
daimessdn@gmail.com <daimessdn@gmail.com>
2021-01-30 07:23:29 +0700

    init'd assignment on week 2

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+  // Task #1: Create an array of multiples of 5 from 125 to 800
+  $multiple_array_of_5 = range(125, 800, 5);
+
+  // Task #2: Print the array from question 1
+  echo $multiple_array_of_5;
+
+?>
