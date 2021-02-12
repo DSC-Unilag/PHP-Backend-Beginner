c9e8e22f94c76559dba2b78cea0bee7a0df214e2
Zubs <zubairidrisaweda@gmail.com>
2021-02-12 17:21:42 +0100

    Week 3 done

--- class/forandforeach.php
+++ class/forandforeach.php
@@ -5,12 +5,26 @@
 	// print_r($letters);
 
 	// For loop
-	for ($counter = 0; $counter < 10; $counter++) { 
-		echo $counter . "<br>";
-	}
+	// for ($counter = 0; $counter < 10; $counter++) { 
+	// 	echo $counter . "<br>";
+	// }
 
-	// Foreach loop
-	foreach ($letters as $index => $letter) {
-		echo $index . " - " . $letter . "<br>";
+	// // Foreach loop
+	// foreach ($letters as $index => $letter) {
+	// 	echo $index . " - " . $letter . "<br>";
+	// }
+
+	for ($counter = 1; $counter < 101; $counter++) { 
+		// Continue
+		if ($counter % 3 != 0) {
+			Continue;
+		}
+
+		// Break
+		if ($counter == 15) {
+			Break;
+		}
+
+		echo $counter . "<br>";
 	}
 ?>
